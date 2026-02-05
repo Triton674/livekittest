4bbd74c71a482f3da03b0718e15eb26fbf6ee763
tristan <tristan.deltour@gmail.com>
2026-02-05 03:59:44 +0100

    fix server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
-import { AccessToken, RoomGrant } from "livekit-server-sdk";
+import { AccessToken, VideoGrant } from "livekit-server-sdk"; // <-- VideoGrant ici
 
 const app = express();
 app.use(cors());
@@ -11,7 +11,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "public")));
 
-// LiveKit credentials depuis .env ou Railway secrets
 const LIVEKIT_API_KEY = process.env.LIVEKIT_API_KEY;
 const LIVEKIT_API_SECRET = process.env.LIVEKIT_API_SECRET;
 const LIVEKIT_URL = process.env.LIVEKIT_URL; // ex: wss://livekit-production-34a9.up.railway.app
@@ -20,13 +19,11 @@ app.get("/token", (req, res) => {
   const role = req.query.role || "listener";
   const roomName = req.query.room || "testroom";
 
-  // Crée un token LiveKit
   const at = new AccessToken(LIVEKIT_API_KEY, LIVEKIT_API_SECRET, {
     identity: role + "_" + Math.floor(Math.random() * 1000),
   });
 
-  const grant = new RoomGrant({ room: roomName });
-  if (role === "admin") grant.setCanPublish(true);
+  const grant = new VideoGrant({ room: roomName }); // <-- VideoGrant
   at.addGrant(grant);
 
   const token = at.toJwt();
